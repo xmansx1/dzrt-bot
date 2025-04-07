c1cae018bf2660ad31f514237209ed1d689a868d
Mansour <xmansx1122@gmail.com>
2025-04-07 18:51:17 +0300

    ✅ تثبيت google-chrome + تحديد المسار اليدوي

--- product_monitor_bot.py
+++ product_monitor_bot.py
@@ -19,13 +19,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/google-chrome"  # ← هذا لازم
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
+     
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
